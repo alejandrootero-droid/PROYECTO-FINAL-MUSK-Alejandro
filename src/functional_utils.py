32e0d33c1622f99c5b57f3e6ca3a77744c2be9a5
alejandrootero-droid <alejandro.otero@ciclosmontecastelo.com>
2026-06-22 16:59:55 +0200

    Add filtering functions for sales by category and client

--- src/functional_utils.py
+++ src/functional_utils.py
@@ -1,1 +1,16 @@
+def filter_sales_by_category(sales, category):
+    return list(
+        filter(
+            lambda sale: sale.category == category,
+            sales
+        )
+    )
 
+
+def filter_sales_by_client(sales, client_id):
+    return list(
+        filter(
+            lambda sale: sale.client_id == client_id,
+            sales
+        )
+    )
