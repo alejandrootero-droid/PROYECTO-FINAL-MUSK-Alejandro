0290cd7c6a556caf3b96ab09bc115c2d9b624a19
alejandrootero-droid <alejandro.otero@ciclosmontecastelo.com>
2026-06-22 17:09:46 +0200

    Add imports and generate_report function in analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -1,1 +1,11 @@
+import json
+import pandas as pd
 
+from src.client import Client
+from src.sale import Sale
+from src.client_collection import ClientCollection
+from src.sales_collection import SalesCollection
+
+
+def generate_report():
+    return {}
