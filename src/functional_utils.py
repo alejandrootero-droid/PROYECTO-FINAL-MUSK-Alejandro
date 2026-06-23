64032e12af45e47d9355a820a12ccb66df4968db
alejandrootero-droid <alejandro.otero@ciclosmontecastelo.com>
2026-06-23 16:45:55 +0200

    Add comments to filter_sales functions
    Updated function definitions to include comments explaining their purpose.

--- src/functional_utils.py
+++ src/functional_utils.py
@@ -1,4 +1,4 @@
-def filter_sales_by_category(sales, category):
+def filter_sales_by_category(sales, category):                           # Filtra las ventas por categoría. -filter- es una función de python para quedarnos solo con lo que queremos de una lista.
     return list(
         filter(
             lambda sale: sale.category == category,
@@ -7,7 +7,7 @@ def filter_sales_by_category(sales, category):
     )
 
 
-def filter_sales_by_client(sales, client_id):
+def filter_sales_by_client(sales, client_id):                            # Lo mismo que la primera funcón pero filtra por la ID del cliente no la categoria del producto.         
     return list(
         filter(
             lambda sale: sale.client_id == client_id,
